6cc0c0866a3af468c405d2c44305fcd857de61d9
Emelin Yuan Lorin <emelinyuan@gmail.com>
2022-10-30 19:38:15 +0700

    initial commit

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php
+
+    echo "<center> hello Word </center>";
+    
+    $nama = "EMELIN YUAN LORIN";
+    $umur = 21;
+
+    echo "Nama saya <strong> $nama</strong>, Saya berusia $umur Tahun<br>";
+    $namaadik = "JESICA WIDA DANISSA";
+    $umuradik = 17;
+
+    echo "Nama adek Saya <strong> $namaadik </strong>, adik saya berusia $umuradik tahun<br>";
+
+    $selisihumur =  $umur-$umuradik;
+    echo "selisih usia saya adalah $selisihumur tahun"
+
+    ?>
